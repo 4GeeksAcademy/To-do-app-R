cf5bc49c57e553e8625d01514b5ab46a89ee366c
Federico Requena Muxí <federico.requena@gmail.com>
2024-10-30 19:53:31 +0000

    initial commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
+	const [inputValue, setInputValue] = useState("");
+	const [todos, setTodos] = useState([]);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>My Todos</h1>
+			<ul>
+				<li>
+					<input 
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)} // Corregido: se pasa e como argumento
+						value={inputValue}
+						onKeyDown={(e) => { // Cambiado a onKeyDown
+							console.log("key was pressed", e.key);
+							if (e.key === "Enter") {
+								setTodos([...todos, inputValue]); // Actualiza todos con el nuevo elemento
+								setInputValue(""); // Limpia el campo después de agregar el todo
+							}
+						}}
+						placeholder="What do you need to do?"
+					/>
+				</li>
+				{todos.map((item, index) => (
+					<li key={index} style={{ display: "flex", alignItems: "center" }}>
+					{item} 
+					<i 
+						className="fas fa-trash-alt" 
+						onClick={() => setTodos(todos.filter((t, currentIndex) => currentIndex !== index))}
+						style={{ marginLeft: "10px", cursor: "pointer" }} // Agregado margen a la izquierda
+					></i>
+				</li>
+				))}
+			</ul>
+
+			<div>{todos.length} tasks</div>
 		</div>
 	);
 };
